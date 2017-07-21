cad75603a9c56612f58241728ffa2a898610e475
zhaoshq <523618033@qq.com>
2017-07-21 17:41:27 +0800

    用测试-测试-设置锁来实现自旋锁

--- src/TTASTestLock.java
+++ src/TTASTestLock.java
@@ -9,7 +9,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
  * 缺点是当锁争用激烈的时候，第二步一直获取不到锁，getAndSet底层使用CAS来实现，一直在修改共享变量的值，会引发缓存一致性流量风暴
  * Created by zhaoshq on 2017/7/20.
  */
-public class TTASLock implements Lock {
+public class TTASTestLock implements TestLock {
 
     private AtomicBoolean mutex = new AtomicBoolean(false);
     @Override
@@ -32,6 +32,6 @@ public class TTASLock implements Lock {
     }
 
     public String toString(){
-        return "TTASLock";
+        return "TTASTestLock";
     }
 }
