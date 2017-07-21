96e2ae384eac3c9ffa87d414ad7318f76c6613fe
zhaoshq <523618033@qq.com>
2017-07-21 17:14:55 +0800

    对内置锁的阻塞队列作了一些优化

--- src/BoundedBuffer.java
+++ src/BoundedBuffer.java
@@ -37,6 +37,7 @@ public class BoundedBuffer<V> {
         while (isEmpty()){
             wait();
         }
+        boolean wasFull = isFull();
 
         V item = buf[head];
         /*
@@ -49,8 +50,11 @@ public class BoundedBuffer<V> {
             head = 0;
         }
         count--;
-        //通知条件队列有元素出列
-        notifyAll();
+
+        if (wasFull){
+            //通知条件队列有元素出列
+            notifyAll();
+        }
         return item;
     }
 
@@ -59,14 +63,22 @@ public class BoundedBuffer<V> {
         while (isFull()){
             wait();
         }
+        boolean wasEmpty = isEmpty();
 
         buf[tail] = item;
         if (++tail == buf.length){
             tail = 0;
         }
         count++;
-        //通知条件队列有元素入列
-        notifyAll();
+        /*
+        * 这里作了一个优化，当缓存从空变为非空时，才发通知，这叫做 “条件通知”
+        * 因为notifyAll是一个重的方法，它会带来大量的上下文切换和锁竞争。
+        * 而notify只有在单进单出，且所有等待线程的类型都相同时才能用，否则会出现丢失信号的问题
+        * */
+        if (wasEmpty){
+            //通知条件队列有元素入列
+            notifyAll();
+        }
     }
 }
 
